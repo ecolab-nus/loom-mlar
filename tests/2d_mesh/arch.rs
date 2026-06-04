fc894e17737724a5494b4ee66b8815cdbf56f6df
heru wang <heru.wang@nus.edu.sg>
2026-06-04 10:25:55 +0000

    fix: fix bcst bandwidth

--- tests/2d_mesh/arch.rs
+++ tests/2d_mesh/arch.rs
@@ -262,7 +262,7 @@ pub fn scaled_mesh_torus() -> Architecture {
             .simple_time_cost(
                 expr("344"),
                 expr("M * N * 2 * effective_bandwidth"),
-                expr("216"),
+                expr("150"),
             )
             .build();
         pm.validate().expect("bcst perf model should validate");
